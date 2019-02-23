cfd4a18874dcc6dfdf736fe77cf93aa23be1891e
Joan-SHI <qiongnashi@gmail.com>
2019-02-24 12:34:27 +1300

    edit friends.js

--- client/actions/friends.js
+++ client/actions/friends.js
@@ -24,32 +24,6 @@ export const creatingData = () => {
     }
   }
   
-  import request from 'superagent'
-
-export const SHOW_ERROR = 'SHOW_ERROR'
-export const CREATE_DATA = 'CREATE_DATA'
-export const CREATED_DATA = 'CREATED_DATA'
-
-export const creatingData = () => {
-    return {
-      type: CREATE_DATA
-    }
-  }
-  
-  export const createdData = (data) => {
-    return {
-      type: CREATED_DATA,
-      createdMessage: data
-    }
-  }
-  
-  export const showError = (errorMessage) => {
-    return {
-      type: SHOW_ERROR,
-      errorMessage: errorMessage
-    }
-  }
-  
   export function CreateData (data) {
     return (dispatch) => {
       dispatch(creatingData())

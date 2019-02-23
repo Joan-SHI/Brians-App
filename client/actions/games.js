7d46afe7aac10181239f674245593f67d020ce35
Joan-SHI <qiongnashi@gmail.com>
2019-02-24 12:50:01 +1300

    edit games.js

--- client/actions/games.js
+++ client/actions/games.js
@@ -1,17 +1,42 @@
 import request from 'superagent'
-import {saveUserToken} from '../utils/auth'
-import {receiveLogin, loginError} from './login'
 
-export function registerUserRequest (creds) {
-  return (dispatch) => {
-    request
-      .post('/api/auth/register')
-      .send(creds)
-      .then(res => {
-        const userInfo = saveUserToken(res.body.token)
-        dispatch(receiveLogin(userInfo))
-        document.location = "/#/"
-      })
-      .catch(err => dispatch(loginError(err.response.body.message)))
+export const SHOW_ERROR = 'SHOW_ERROR'
+export const CREATE_DATA = 'CREATE_DATA'
+export const CREATED_DATA = 'CREATED_DATA'
+
+export const creatingData = () => {
+    return {
+      type: CREATE_DATA
+    }
   }
-}
+  
+  export const createdData = (data) => {
+    return {
+      type: CREATED_DATA,
+      createdMessage: data
+    }
+  }
+  
+  export const showError = (errorMessage) => {
+    return {
+      type: SHOW_ERROR,
+      errorMessage: errorMessage
+    }
+  }
+  
+  export function CreateData (data) {
+    return (dispatch) => {
+      dispatch(creatingData())
+      return request
+        .post(`/api/v1/user/create`)
+        .send(data)
+        .then(res => {
+            console.log(res.body);
+            
+          dispatch(createdData(res.body))
+        })
+        .catch(err => {
+          dispatch(showError(err.message))
+        })
+    }
+  }
